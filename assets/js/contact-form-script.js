3612a7141bb6924bf239525c04c8478235eee76d
ashish13377 <trojan2277@gmail.com>
2022-06-08 18:37:29 +0530

    new update

--- assets/js/contact-form-script.js
+++ assets/js/contact-form-script.js
@@ -10,8 +10,7 @@
             submitMSG(false, "Did you fill in the form properly?");
         } else {
             // everything looks good!
-            event.preventDefault();
-            submitForm();
+    
         }
     });
 
